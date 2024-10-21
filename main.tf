2f2d7044de2dd74297144b4e85210accb22c02df
Abhishek kurmatkar <97021984+Abhi84481@users.noreply.github.com>
2024-10-21 19:55:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,6 @@ terraform {
 provider "google" {
   project = "prj-adc-gcp-devsecops"
   region  = "me-central2"
-  # Workload Identity automatically uses the correct service account from Kubernetes
 }
 
 data "google_project" "my_project" {
